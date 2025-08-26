fe7aba0cac965dfb19bf786ca2dd60cbd4e88ec6
Andres Olave <arolave@gmail.com>
2025-08-26 13:03:12 +1000

    fix: references to setup-tests in samples and sdks
    Signed-off-by: Andres Olave <arolave@gmail.com>

--- samples/sample-web-wallet-app/setup-tests.js
+++ samples/sample-web-wallet-app/setup-tests.js
@@ -14,11 +14,16 @@
  * limitations under the License.
  *
  */
-import { describe, it } from 'node:test';
-import { expect } from 'expect';
+import { register } from 'node:module';
+import { afterEach } from 'node:test';
+import { cleanup } from '@testing-library/react';
 
-describe('Empty Test', () => {
-  it('empty test', async () => {
-    expect(true).toBeTruthy();
-  });
-});
+register('../../tooling/resolvers/css-resolver.mjs', import.meta.url);
+register('../../tooling/resolvers/jsx-loader.mjs', import.meta.url);
+
+afterEach(cleanup);
+
+// eslint-disable-next-line better-mutation/no-mutation,no-undef
+global.Event = window.Event;
+// eslint-disable-next-line better-mutation/no-mutation,no-undef
+global.CustomEvent = window.CustomEvent;
