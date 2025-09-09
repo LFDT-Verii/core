4263ad6ba7c49347885caabdc9f0d77178b1d1c5
ItayPodhajcer <19306955+ItayPodhajcer@users.noreply.github.com>
2025-09-09 13:21:21 +0300

    fix: Lint fixes
    Signed-off-by: ItayPodhajcer <19306955+ItayPodhajcer@users.noreply.github.com>

--- packages/http-client/src/client.js
+++ packages/http-client/src/client.js
@@ -116,7 +116,7 @@ const initHttpClient = (options) => {
         statusCode,
         resHeaders,
         json: async () => {
-          if (method == 'POST' && !rawBody.bodyUsed) {
+          if (method === 'POST' && !rawBody.bodyUsed) {
             return {};
           }
 
