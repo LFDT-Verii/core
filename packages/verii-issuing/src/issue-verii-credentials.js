e4553313739986a60bf49509f4a6f0f08f6d3f0c
Andres Olave <arolave@gmail.com>
2025-10-28 14:52:15 +1100

    fix: add documentation
    Signed-off-by: Andres Olave <arolave@gmail.com>

--- packages/verii-issuing/src/issue-verii-credentials.js
+++ packages/verii-issuing/src/issue-verii-credentials.js
@@ -127,7 +127,7 @@ const anchorVeriiCredentials = async (credentialMetadatas, issuer, context) => {
 };
 
 /**
- * Gets the new list entry
+ * Gets the new list entry. Since the number of entries per list is 10k then only one will ever be returned
  * @param {AllocationListEntry[]} entries the entries
  * @returns {AllocationListEntry} returns the new list entry if it exists, otherwise undefined
  */
